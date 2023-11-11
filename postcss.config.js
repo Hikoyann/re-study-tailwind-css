85a1734a4408b23d85c6139f99ca7bc16948065d
Hikoyann <i21i022dl@ous.jp>
2023-11-11 14:39:22 +0900

    Tailwind CSSの環境設定について学習

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
-module.exports = {
-  plugins: {
+module.exports = (ctx) => {
+  return {
+    plugins: {
     tailwindcss: {},
     autoprefixer: {},
-  },
-}
+    cssnano: ctx.env === "production" ? {} : false,
+    }
+  };
+};
